64101fc8de46c846afc201e68e4a022d229af543
Jose Sian <chepe_sian@icloud.com>
2023-02-15 09:08:36 -0600

    Create project
    Create project

---
+++ paytm/src/test/java/com/javatechie/spring/paytm/api/PaytmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.paytm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
